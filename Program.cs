7f3bac76adeb5746977687e7fc383021de0b7b8c
FIC <stevamlima99@gmail.com>
2017-10-05 21:01:06 -0300

    alteração código

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleCadastro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FazerCafe("caramelo");
+            FazerCafe("chocolate");
+
+        }
+
+        static void FazerCafe(string capsula)
+        {
+            System.Console.WriteLine("Esquentar agua");
+            System.Console.WriteLine("Colocar pó de magia(café)");
+            System.Console.WriteLine("Colocar " + capsula);
+
+        }
+
+
+
+
+    }
+}
